0fc6277018f18dacb9e2baf09a056e4e5e5df22b
dwcoates <dodge.w.coates@gmail.com>
2017-03-24 00:47:46 -0400

    correct bug that left tasks without bodies

--- org-capture-builder.el
+++ org-capture-builder.el
@@ -113,7 +113,7 @@ NULL use for plist extraction."
       (list
        ;; tasks
        (t-wrapper (plist-get basic :todo) global-tags (concat prefix "t") desc loc nil
-                  "todo" t   nil nil t   nil nil nil nil)
+                  "todo" t   nil nil t t nil nil nil)
 
        ;; idea
        (t-wrapper (plist-get basic :idea) global-tags (concat prefix "i") desc loc '("idea")
